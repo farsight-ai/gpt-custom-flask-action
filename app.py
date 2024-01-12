e3658178ffd35a294187ca232da414acc57ca80a
nfaro@cogolabs.com <nfaro@cogolabs.com>
2024-01-12 11:26:01 -0500

    Removal of HTML Tags + Whitespace

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, make_response, request
 from flask_cors import CORS
 import requests
+import re
 
 app = Flask(__name__)
 CORS(app, supports_credentials=True)
@@ -10,6 +11,10 @@ CORS(app, supports_credentials=True)
 def health_check():
     return make_response("Healthy.", 200)
 
+def remove_html_tags_and_whitespace(html):
+    content = re.sub(r"\<.*?\>|[\t\n]", "", html)
+    return content
+
 # Fetches HTML from any given url. Expects the url to be a query parameter.
 @app.route('/fetch-html', methods=["GET"])
 def fetch_html():
@@ -18,8 +23,9 @@ def fetch_html():
     html_text = page.text
     encoded_html = html_text.encode(page.encoding)
     decoded_html = encoded_html.decode(page.encoding)
+    html_content = remove_html_tags_and_whitespace(decoded_html)
 
-    return make_response({"html": decoded_html}, 200)
+    return make_response({"html": html_content}, 200)
 
 if __name__ == "__main__":
-    app.run('0.0.0.0', port=5000)
+    app.run('127.0.0.1', port=5000)
